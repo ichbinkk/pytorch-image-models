bf94fe31117b2a11bb8fecbe8f095c2b61af4317
ichbinkk <493208449@qq.com>
2021-08-14 00:46:52 +0800

    change --read from txt file

--- timm/data/parsers/parser_image_folder.py
+++ timm/data/parsers/parser_image_folder.py
@@ -14,28 +14,28 @@ from .class_map import load_class_map
 from .constants import IMG_EXTENSIONS
 
 
-def find_images_and_targets(folder, types=IMG_EXTENSIONS, class_to_idx=None, leaf_name_only=True, sort=True):
-    labels = []
-    filenames = []
-    for root, subdirs, files in os.walk(folder, topdown=False, followlinks=True):
-        rel_path = os.path.relpath(root, folder) if (root != folder) else ''
-        label = os.path.basename(rel_path) if leaf_name_only else rel_path.replace(os.path.sep, '_')
-        for f in files:
-            base, ext = os.path.splitext(f)
-            if ext.lower() in types:
-                filenames.append(os.path.join(root, f))
-                labels.append(label)
-    if class_to_idx is None:
-        # building class index
-        unique_labels = set(labels)
-        sorted_labels = list(sorted(unique_labels, key=natural_key))
-        class_to_idx = {c: idx for idx, c in enumerate(sorted_labels)}
-    images_and_targets = [(f, class_to_idx[l]) for f, l in zip(filenames, labels) if l in class_to_idx]
-    if sort:
-        images_and_targets = sorted(images_and_targets, key=lambda k: natural_key(k[0]))
-    return images_and_targets, class_to_idx
+# def find_images_and_targets(folder, types=IMG_EXTENSIONS, class_to_idx=None, leaf_name_only=True, sort=True):
+#     labels = []
+#     filenames = []
+#     for root, subdirs, files in os.walk(folder, topdown=False, followlinks=True):
+#         rel_path = os.path.relpath(root, folder) if (root != folder) else ''
+#         label = os.path.basename(rel_path) if leaf_name_only else rel_path.replace(os.path.sep, '_')
+#         for f in files:
+#             base, ext = os.path.splitext(f)
+#             if ext.lower() in types:
+#                 filenames.append(os.path.join(root, f))
+#                 labels.append(label)
+#     if class_to_idx is None:
+#         # building class index
+#         unique_labels = set(labels)
+#         sorted_labels = list(sorted(unique_labels, key=natural_key))
+#         class_to_idx = {c: idx for idx, c in enumerate(sorted_labels)}
+#     images_and_targets = [(f, class_to_idx[l]) for f, l in zip(filenames, labels) if l in class_to_idx]
+#     if sort:
+#         images_and_targets = sorted(images_and_targets, key=lambda k: natural_key(k[0]))
+#     return images_and_targets, class_to_idx
 
-def find_images_and_targets2(folder, types=IMG_EXTENSIONS, class_to_idx=None, leaf_name_only=True, sort=True):
+def find_images_and_targets(folder, types=IMG_EXTENSIONS, class_to_idx=None, leaf_name_only=True, sort=True):
     labels = []
     filenames = []
     txt_path = os.path.join(folder, '.txt')
