6e40840d084962f38f6745d1cf06e24198704352
ichbinkk <493208449@qq.com>
2022-06-29 12:11:24 +0800

    add matrix 3

--- sum_excel.py
+++ sum_excel.py
@@ -6,7 +6,7 @@ import os
 # Set argparse
 parser = argparse.ArgumentParser(description='Sum_excel')
 
-parser.add_argument('--data_dir', metavar='DIR', default='./output/V4_ec/Ablation',
+parser.add_argument('--data_dir', metavar='DIR', default='./output/V4_ec_3',
                     help='path to excels')
 
 
@@ -36,23 +36,46 @@ def create_matrix2(data, model_name):
             matrix2[i+1].append(a[1])
 
 
+def create_matrix3(data, model_name):
+    sheet = data.worksheets[3]
+    table = list(sheet.values)
+
+    if len(matrix3) == 0:
+        matrix3.append([model_name+'_train_loss', 'test_loss'])
+        for line in table:
+            a = list(line)
+            matrix3.append(a)
+    else:
+        matrix3[0].append(model_name+'_train_loss')
+        matrix3[0].append('test_loss')
+        for i, line in enumerate(table):
+            a = list(line)
+            matrix3[i+1].append(a[0])
+            matrix3[i + 1].append(a[1])
+
 if __name__ == "__main__":
     # get all args params
     args = parser.parse_args()
     out_file = os.path.join(args.data_dir, 'result.xlsx')
 
+    # Global var
     matrix1 = []
     matrix2 = []
+    matrix3 = []
     for root, dirs_name, files_name in os.walk(args.data_dir):
         for i in files_name:
             if i.split('.')[-1] in ['xlsx']:
                 file_name = os.path.join(root, i)
-                # for sheet 1
                 data = openpyxl.load_workbook(file_name)
+
+                # for sheet 1
                 model_name = create_matrix1(data)
 
                 # for sheet 2
                 create_matrix2(data, model_name)
+
+                # for sheet 2
+                create_matrix3(data, model_name)
 
 
     # matrix_t = list(map(list,zip(*matrix)))
@@ -60,8 +83,11 @@ if __name__ == "__main__":
     wb = openpyxl.Workbook()
     s1 = wb.get_sheet_by_name('Sheet')
     s2 = wb.create_sheet('Sheet2')
+    s3 = wb.create_sheet('Sheet3')
     for line in matrix1:
         s1.append(line)
     for line in matrix2:
         s2.append(line)
+    for line in matrix3:
+        s3.append(line)
     wb.save(out_file)
