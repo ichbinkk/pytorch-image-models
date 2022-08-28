d6109525f6ad57cc7a8cb2e930afda9f97d9ed29
ichbinkk <493208449@qq.com>
2022-08-28 10:35:38 +0800

    temp

--- sum_excel.py
+++ sum_excel.py
@@ -6,7 +6,7 @@ import os
 # Set argparse
 parser = argparse.ArgumentParser(description='Sum_excel')
 
-parser.add_argument('--data_dir', metavar='DIR', default='./output/V4_ec/3',
+parser.add_argument('--data_dir', metavar='DIR', default='./output/V5_ec',
                     help='path to excels')
 
 
